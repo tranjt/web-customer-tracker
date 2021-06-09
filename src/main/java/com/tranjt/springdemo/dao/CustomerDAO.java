656e626083a56c9a33bf1550feb0d87a2227e8ba
JTTran <tranjt80@gmail.com>
2021-06-10 00:36:47 +0300

    add customer Hibernate DAO

---
+++ src/main/java/com/tranjt/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.tranjt.springdemo.dao;
+
+import java.util.List;
+
+import com.tranjt.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+}
